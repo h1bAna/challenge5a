f4308f1c403ffee53e43254276664ba67d20d602
h1bAna <ba.24.04.2002@gmail.com>
2024-01-03 00:20:28 +0700

    first commit

---
+++ source/login.php
@@ -0,0 +1,128 @@
+<?php
+
+define( 'LMS_WEB_PAGE_TO_ROOT', '' );
+require_once LMS_WEB_PAGE_TO_ROOT . 'resources/includes/lmsPage.inc.php';
+
+dvwaDatabaseConnect();
+
+if( isset( $_POST[ 'Login' ] ) ) {
+	// Anti-CSRF
+	if (array_key_exists ("session_token", $_SESSION)) {
+		$session_token = $_SESSION[ 'session_token' ];
+	} else {
+		$session_token = "";
+	}
+
+    // Check Anti-CSRF token
+    checkToken( $_REQUEST[ 'user_token' ], $_SESSION[ 'session_token' ], 'index.php' );
+
+    // Sanitise username input
+    $user = $_POST[ 'username' ];
+    $user = stripslashes( $user );
+    $user = ((isset($GLOBALS["___mysqli_ston"]) && is_object($GLOBALS["___mysqli_ston"])) ? mysqli_real_escape_string($GLOBALS["___mysqli_ston"],  $user ) : ((trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR)) ? "" : ""));
+
+    // Sanitise password input
+    $pass = $_POST[ 'password' ];
+    $pass = stripslashes( $pass );
+    $pass = ((isset($GLOBALS["___mysqli_ston"]) && is_object($GLOBALS["___mysqli_ston"])) ? mysqli_real_escape_string($GLOBALS["___mysqli_ston"],  $pass ) : ((trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR)) ? "" : ""));
+    $pass = md5( $pass );
+
+	$query  = "SELECT * FROM `users` WHERE user='$user' AND password='$pass';";
+	$result = @mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '.<br /> Something wrong with database.</pre>' );
+	if( $result && mysqli_num_rows( $result ) == 1 ) {    // Login Successful...
+		dvwaMessagePush( "You have logged in as '{$user}'" );
+		lmsLogin( $user );
+		lmsRedirect( LMS_WEB_PAGE_TO_ROOT . 'index.php' );
+	}
+
+	// Login failed
+	dvwaMessagePush( 'Login failed' );
+	lmsRedirect( 'login.php' );
+}
+
+$messagesHtml = messagesPopAllToHtml();
+
+Header( 'Cache-Control: no-cache, must-revalidate');    // HTTP/1.1
+Header( 'Content-Type: text/html;charset=utf-8' );      // TODO- proper XHTML headers...
+Header( 'Expires: Tue, 23 Jun 2009 12:00:00 GMT' );     // Date in the past
+
+// Anti-CSRF
+generateSessionToken();
+
+echo "<!DOCTYPE html>
+
+<html lang=\"en-GB\">
+
+	<head>
+
+		<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />
+
+		<title>Login</title>
+
+		<link rel=\"stylesheet\" type=\"text/css\" href=\"" . LMS_WEB_PAGE_TO_ROOT . "resources/css/login.css\" />
+
+	</head>
+
+	<body>
+
+	<div id=\"wrapper\">
+
+	<div id=\"header\">
+
+	<br />
+
+	<p><img src=\"" . LMS_WEB_PAGE_TO_ROOT . "resources/images/logo_kma.png\" /></p>
+
+	<br />
+
+	</div> <!--<div id=\"header\">-->
+
+	<div id=\"content\">
+
+	<form action=\"login.php\" method=\"post\">
+
+	<fieldset>
+
+			<label for=\"user\">Tên đăng nhập</label> <input type=\"text\" class=\"loginInput\" size=\"20\" name=\"username\"><br />
+
+
+			<label for=\"pass\">Mật khẩu</label> <input type=\"password\" class=\"loginInput\" AUTOCOMPLETE=\"off\" size=\"20\" name=\"password\"><br />
+
+			<br />
+
+			<p class=\"submit\"><input type=\"submit\" value=\"Đăng nhập\" name=\"Login\"></p>
+
+	</fieldset>
+
+	" . tokenField() . "
+
+	</form>
+
+	<br />
+
+	{$messagesHtml}
+
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+    <br />
+	</div > <!--<div id=\"content\">-->
+
+	<div id=\"footer\">
+    <p style=\"font-size:13px;\" >Learning management system!</p>
+	<p>" . dvwaExternalLinkUrlGet( 'https://github.com/digininja/DVWA/', 'Damn Vulnerable Web Application (DVWA)' ) . "</p>
+
+	</div> <!--<div id=\"footer\"> -->
+
+	</div> <!--<div id=\"wrapper\"> -->
+
+	</body>
+
+</html>";
+
+?>
